9c35779ae1980352d3cdb2d85552afd8cfd6d585
Damix222 <spiridonovaroslav004@gmail.com>
2026-04-30 18:44:39 +0300

    Add temporary task endpoints

--- app/main.py
+++ app/main.py
@@ -1,9 +1,38 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
+
+tasks = []
 
 
 @app.get("/")
 def check_status():
-    return {'message': "Task Tracker API is running"}
+    return {"message": "Task Tracker API is running"}
 
+
+@app.get("/tasks")
+def get_tasks():
+    return tasks
+
+
+@app.post("/tasks")
+def create_task(title: str, description: str):
+    task = {
+        "id": len(tasks) + 1,
+        "title": title,
+        "description": description,
+        "completed": False
+    }
+
+    tasks.append(task)
+
+    return task
+
+
+@app.get("/tasks/{task_id}")
+def read_task(task_id: int):
+    for task in tasks:
+        if task["id"] == task_id:
+            return task
+
+    raise HTTPException(status_code=404, detail="Task not found")
