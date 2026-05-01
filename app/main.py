12f8cfbb6f2a81e5b23b8ece6fbe34bd34a3a800
Damix222 <spiridonovaroslav004@gmail.com>
2026-05-01 20:24:48 +0300

    Use SQLite database for task endpoints

--- app/main.py
+++ app/main.py
@@ -1,13 +1,22 @@
-from fastapi import FastAPI, HTTPException
-from app.schemas import TaskCreate, TaskResponse
-from app.database import engine
+from fastapi import FastAPI, HTTPException, Depends
+from sqlalchemy.orm import Session
+
 from app import models
+from app.database import engine, SessionLocal
+from app.schemas import TaskCreate, TaskResponse
+
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-tasks = []
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 @app.get("/")
@@ -16,28 +25,30 @@ def check_status():
 
 
 @app.get("/tasks", response_model=list[TaskResponse])
-def get_tasks():
-    return tasks
+def get_tasks(db: Session = Depends(get_db)):
+    return db.query(models.Task).all()
 
 
 @app.post("/tasks", response_model=TaskResponse, status_code=201)
-def create_task(task_data: TaskCreate):
-    task = {
-        "id": len(tasks) + 1,
-        "title": task_data.title,
-        "description": task_data.description,
-        "completed": False
-    }
+def create_task(task_data: TaskCreate, db: Session = Depends(get_db)):
+    db_task = models.Task(
+        title=task_data.title,
+        description=task_data.description,
+        completed=False
+    )
 
-    tasks.append(task)
+    db.add(db_task)
+    db.commit()
+    db.refresh(db_task)
 
-    return task
+    return db_task
 
 
 @app.get("/tasks/{task_id}", response_model=TaskResponse)
-def read_task(task_id: int):
-    for task in tasks:
-        if task["id"] == task_id:
-            return task
+def read_task(task_id: int, db: Session = Depends(get_db)):
+    db_task = db.query(models.Task).filter(models.Task.id == task_id).first()
 
-    raise HTTPException(status_code=404, detail="Task not found")
+    if db_task is None:
+        raise HTTPException(status_code=404, detail="Task not found")
+
+    return db_task
